1577fe66925b0daba886af3c89017c478735b67e
Matt McDermott <41551029+mcdermatt@users.noreply.github.com>
2019-11-21 11:04:17 -0500

    updated 1linkGravComp

--- 1linkGravComp.py
+++ 1linkGravComp.py
@@ -5,7 +5,11 @@ import time
 import math
 import numpy as np
 
-l2m = 6.5 #kg
+l2m = 1.85 #kg
+l2com = 0.1893 #m
+l2cpr = 8192 
+l2reduction = 6
+l2kv = 100
 
 
 #calibration
@@ -20,4 +24,17 @@ od.axis0.controller.config.control_mode = CTRL_MODE_CURRENT_CONTROL
 od.axis0.requested_state = AXIS_STATE_CLOSED_LOOP_CONTROL
 
 while True:
+	#get joint angle
 	pos = od.axis0.encoder.pos_estimate
+
+	#zero position is straight up
+	theta = (2 * numpy.pi * pos) / (l2cpr * l2reduction)
+	force = l2m*9.81*l2com*np.sin(theta)
+
+	currentSetpoint = (force * l2kv) / 8.27
+	od.axis0.controller.current_setpoint = currentSetpoint
+
+	print("Theta: ",theta,"   Current Setpoint: ", currentSetpoint)
+	time.sleep(0.01)
+	
+
