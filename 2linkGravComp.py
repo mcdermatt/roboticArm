2bc4108d45ae89e2f3cbf5d2426792208e5c5d0f
Matt McDermott <41551029+mcdermatt@users.noreply.github.com>
2019-11-25 13:37:00 -0500

    flipped sign of vel1

--- 2linkGravComp.py
+++ 2linkGravComp.py
@@ -9,14 +9,14 @@ import numpy as np
 #system parameters
 
 #l1- shoulder up/down
-l1m = 40 #kg was 3.07
+l1m = 80 #kg was 3.07
 l1com = 0.0729 #m from center of rotation
 l1 = 0.1651
 l1cpr = 910
 l1reduction = 9
 l1kv = 16
 #beta1 = 0
-beta1 = -0.0025 #to do- make beta a function motor velocity to cancel out inertia?
+beta1 = -0.005 #to do- make beta a function motor velocity to cancel out inertia?
 serial1 = "2084377D3548"
 
 #l2- elbow
@@ -61,7 +61,7 @@ while True:
 	pos2 = od2.axis0.encoder.pos_estimate
 	vel2 = od2.axis0.encoder.vel_estimate
 	pos1 = (-1)*od1.axis0.encoder.pos_estimate
-	vel1 = od1.axis0.encoder.vel_estimate
+	vel1 = (-1)*od1.axis0.encoder.vel_estimate
 
 	#zero position is straight up
 	theta2 = (2 * np.pi * pos2) / (l2cpr * l2reduction)
@@ -80,7 +80,7 @@ while True:
 	
 	force1 = (l1m*9.81*l1com*np.sin(theta1)) + (l2m*9.81*(l1*np.sin(theta1)+l2com*np.sin(theta2eff))) + beta1*vel1
 	currentSetpoint1 = (-1 * l1kv*force1)/(8.27*l1reduction)
-	measuredCurrent1 = od1.axis0.motor.current_meas_phB
+	measuredCurrent1 = od1.axis0.motor.current_control.Iq_measured
 
 	od1.axis0.controller.current_setpoint = currentSetpoint1
 
@@ -88,7 +88,7 @@ while True:
 	error1 = od1.axis0.error
 
 
-	print("Theta2eff: ",theta2eff,"   Theta1: ", theta1,"   CSJ2: ", currentSetpoint2, " CSJ1:  ", currentSetpoint1, " CMJ1: ", measuredCurrent1, error1)
+	print("Theta2eff: ",theta2eff,"   Theta1: ", theta1," CSJ2: ", currentSetpoint2, " CSJ1: ", currentSetpoint1, " CMJ1: ", measuredCurrent1, error1)
  	
 
 
