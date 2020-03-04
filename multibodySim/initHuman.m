9a2050fca0a03e68b69372db553313895b1bef25
mcdermatt <mcdermatt@gmail.com>
2020-03-03 19:22:37 -0800

    added hips and chicken wing

---
+++ multibodySim/initHuman.m
@@ -0,0 +1,22 @@
+j0pi = 0;
+j1pi = 0;
+j2pi = 0;
+
+j0vi = 0;
+j1vi = 0;
+j2vi = 0;
+
+fx = [0 0];
+fy = [0 0];
+fz = [0 0];
+
+hipsll = -45;
+hipsul = 45;
+j0ll = -90;
+j0ul = 90;
+cwll = 0;
+cwul = 90;
+j1ll = -180;
+j1ul = 180;
+j2ll = -130;
+j2ul = 5;
