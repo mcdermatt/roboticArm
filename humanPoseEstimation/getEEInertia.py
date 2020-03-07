44ded17bb963f15c87249fc14802bf9416f86486
mcdermatt <mcdermatt@gmail.com>
2020-03-07 11:00:55 -0800

    more work on getEEInertia()

--- humanPoseEstimation/getEEInertia.py
+++ humanPoseEstimation/getEEInertia.py
@@ -1,4 +1,4 @@
-import numpy as numpy
+import numpy as np
 
 def getEEInertia(x,y,z):
 	"""Get End Effector Inertia at point XYZ assumes ZERO VELOCITY"""
@@ -6,15 +6,45 @@ def getEEInertia(x,y,z):
 	table = "../multibodySim/ellAxis.txt" #from MatLab SimScape MultiBody
 
 	try:
+		#load in binary file for inertiaTable 
+		#(will not exist if MatLab generates replacement)
 		inertiaTable = np.load('inertiaTable.npy')
 	except:
-		inertiaTable = np.genfromtxt(table, inertiaTable)
+		inertiaTable = np.genfromtxt(table,delimiter=',')
 		np.save('inertiaTable.npy',inertiaTable)
 
+	xmin = -0.5;
+	xmax = 0.5;
+	ymin = 0.25;
+	ymax = 0.85;
+	zmin = 0.2;
+	zmax = 0.5;
+	fidelity = 0.1;
 
-	Ix = 0
-	Iy = 0
-	Iz = 0
+	xPts = np.arange(xmin,xmax+0.0001,fidelity)
+	yPts = np.arange(ymin,ymax+0.0001,fidelity)
+	zPts = np.arange(zmin,zmax+0.0001,fidelity)
+	xCt = 0
+	yCt = 0
+	zCt = 0
+
+	for i in xPts:
+		if i >=x:
+			break
+		xCt =+ 1
+	for i in yPts:
+		if i >=y:
+			break
+		zCt =+ 1
+	for i in zPts:
+		if i >=z:
+			break
+		zCt =+ 1
+
+	address = (xPts.shape[0])*(yPts.shape[0])*xCt + (yPts.shape[0])*yCt + zCt
+	Ix = inertiaTable[0,address]
+	Iy = inertiaTable[1,address]
+	Iz = inertiaTable[2,address]
 
 	inertia = np.array([Ix, Iy, Iz])
 	return(inertia)
