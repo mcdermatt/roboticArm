132f81e721e4bab0604c6f7f07cd590abaec8c4d
mcdermatt <mcdermatt@gmail.com>
2020-03-08 16:33:10 -0700

    fixed guessShoulder

--- humanPoseEstimation/guessShoulder.py
+++ humanPoseEstimation/guessShoulder.py
@@ -4,57 +4,55 @@ import matplotlib.pyplot as plt
 
 forces = np.loadtxt('forces.txt')
 
-#draw xpos-force relationship
+#draw X/Z pos-force relationship
 fig1 = plt.figure(1)
 ax = fig1.add_subplot()
 ax.set_xlabel('x')
-ax.set_ylabel('force magnitude')
+ax.set_ylabel('force magnitude ratio x/z')
 xForces = forces[forces[:,0].argsort()]
 print(xForces.shape)
 count = 0
 while count < forces.shape[1]:
 	xpt = xForces[0,count]
-	ypt = xForces[3,count]
+	ypt = xForces[3,count]/xForces[5,count]
 	
 	plt.plot(xpt,ypt,'bo')
 	count += 1
+plt.autoscale(enable=True, axis='y')
 plt.draw()
 
-#draw ypos-force relationship
+#draw X/Y pos-force relationship
 fig2 = plt.figure(2)
 ax = fig2.add_subplot()
 ax.set_xlabel('y')
-ax.set_ylabel('force magnitude')
-
+ax.set_ylabel('force magnitude ratio y/x')
 xForces = forces[forces[:,1].argsort()]
 print(xForces.shape)
-
 count = 0
 while count < forces.shape[1]:
-	xpt = xForces[1,count]
-	ypt = xForces[4,count]
+	xpt = xForces[0,count]
+	ypt = xForces[4,count]/xForces[3,count]
 	
 	plt.plot(xpt,ypt,'bo')
 	count += 1
+plt.autoscale(enable=True, axis='y')
+plt.draw()
 
-#draw zpos-force relationship
+#draw Y/Z pos-force relationship
 fig3 = plt.figure(3)
 ax = fig3.add_subplot()
 ax.set_xlabel('z')
-ax.set_ylabel('force magnitude')
-
+ax.set_ylabel('force magnitude ratio z/y')
 xForces = forces[forces[:,2].argsort()]
 print(xForces.shape)
-
 count = 0
 while count < forces.shape[1]:
-	xpt = xForces[2,count]
-	ypt = xForces[5,count]
+	xpt = xForces[1,count]
+	ypt = xForces[5,count]/xForces[4,count]
 	
 	plt.plot(xpt,ypt,'bo')
 	count += 1
-
-
+plt.autoscale(enable=True, axis='y')
 plt.draw()
 
-plt.pause(10)
+plt.pause(30)
