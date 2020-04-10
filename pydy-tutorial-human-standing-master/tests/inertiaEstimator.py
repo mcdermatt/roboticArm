6d82b7a3a45bc561cdebf3d0edc491de210ff3cc
mcdermatt <mcdermatt@gmail.com>
2020-04-10 12:52:23 -0400

    got inertiaEstimator func workingish

--- pydy-tutorial-human-standing-master/tests/inertiaEstimator.py
+++ pydy-tutorial-human-standing-master/tests/inertiaEstimator.py
@@ -1,5 +1,6 @@
 import cloudpickle
 from numpy import zeros, array, linspace, deg2rad
+import numpy as np
 from scipy.integrate import odeint
 
 class inertiaEstimator:
@@ -32,7 +33,7 @@ class inertiaEstimator:
 	t = linspace(0.0, final_time, final_time * frames_per_sec)
 
 	x0 = zeros(6)
-	x0[0] = deg2rad(45)
+	x0[0] = deg2rad(90)
 	x0[1] = deg2rad(45)
 	x0[2] = deg2rad(45)
 
@@ -74,3 +75,33 @@ class inertiaEstimator:
 		stateVec[2,:] = self.predictz()[-1]
 
 		return(stateVec)
+
+	def joint2Cartesian(self, theta0, theta1, theta2):
+
+		cart = zeros(3)
+
+		cart[0] = (self.numerical_constants[4]*np.sin(theta1) + 0.2374*np.sin(theta1+theta2)) * np.cos(-1* theta0) #x l2=0.2374
+		cart[1] = self.numerical_constants[4]*np.cos(theta1) + 0.2374*np.cos(theta1+theta2) #y
+		cart[2] = (self.numerical_constants[4]*np.sin(theta1) + 0.2374*np.sin(theta1+theta2)) * np.sin(-1*theta0) #z
+
+		return(cart)
+
+	def getInertia(self):
+		states = self.predict()
+		diff = zeros(3)
+
+		print("x0 = ", self.x0)
+
+		for i in range(0,3):
+		#convert to cartesian space
+			fin = self.joint2Cartesian(states[i,0],states[i,1],states[i,2])
+			print("fin = ",fin)
+			initial = self.joint2Cartesian(self.x0[0],self.x0[1],self.x0[2])
+			print("initial = ", initial)
+			diff[i] = abs(abs(fin[i]) - abs(initial[i]))
+			print("diff = ", diff)
+			#F = Ia
+			#0.1 = I*(diff)
+		inertias = 0.1/diff
+
+		return(inertias)
