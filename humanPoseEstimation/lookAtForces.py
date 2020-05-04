9885e52ada131d0fe6bb51a74c0779367b177378
mcdermatt <mcdermatt@gmail.com>
2020-05-04 16:20:37 -0400

    dangerously smooth

--- humanPoseEstimation/lookAtForces.py
+++ humanPoseEstimation/lookAtForces.py
@@ -22,6 +22,9 @@ forces = sg.getCartForces(pathCart)
 t = np.arange(np.shape(forces)[0])
 
 xForces = forces[:,0]
+yForces = forces[:,2]
+zForces = forces[:,1]
+
 
 #Savgol Filter
 # window_size = 101
@@ -31,23 +34,42 @@ xForces = forces[:,0]
 # print(sg)
 
 plt.figure()
-#Moving Average Filter
-window_size = 30
+#Moving Average Filter - looks like this one's the winner
+window_size = 100
+
 xForcesMA = movingAverage(xForces,window_size)
+yForcesMA = movingAverage(yForces,window_size)
+zForcesMA = movingAverage(zForces,window_size)
 # plt.plot(xForces[len(xForces)-len(yMA):],yMA)
-plt.plot(t[len(t)-len(xForcesMA):],xForcesMA,label="Moving Average Filter")
+plt.plot(t[len(t)-len(xForcesMA):],xForcesMA,label="Moving Average Fx")
+# plt.plot(t[len(t)-len(yForcesMA):],yForcesMA,label="Moving Average Fy")
+# plt.plot(t[len(t)-len(zForcesMA):],zForcesMA,label="Moving Average Fz")
+
+
+print("shape = ",np.shape(xForcesMA))
 
 #Cubic Interpolation of moving average data
 f2 = interp1d(t[len(t)-len(xForcesMA):],xForcesMA,kind='cubic')
-tNew = np.linspace(len(t)-len(xForcesMA),len(xForces)-1,num=41,endpoint=True)
-cubicInterp, = plt.plot(tNew, f2(tNew), '--')
+tNew = np.linspace(len(t)-len(xForcesMA),len(xForces),num=100,endpoint=True)
+# cubicInterp, = plt.plot(tNew, f2(tNew), '--')
 
 # plt.axis([0,np.shape(forces)[0],0,5])
+
 #RAW DATA IS NOISY AF
 ptsX, = plt.plot(t,forces[:,0],'r.', label='Raw Force Data')
 
+#TODO look into spline
+plt.legend(loc='best')
 
+plt.figure()
+plt.axis([-0.5,0.5,0,3])
+plt.plot(pathCart[window_size-1:,0],xForcesMA/zForcesMA,'.')
 
+polyOrder = 4
+bestFitX = np.polyfit(pathCart[window_size-1:,0],xForcesMA/zForcesMA,polyOrder)
+pX = np.poly1d(bestFitX)
+xpX= np.linspace(-1,1,100)
+plt.plot(xpX,pX(xpX),'--')
 
 #OLD POLYFIT STRATEGY
 # polyOrder = 30
@@ -55,6 +77,5 @@ ptsX, = plt.plot(t,forces[:,0],'r.', label='Raw Force Data')
 # pX = np.poly1d(bestFitX)
 # xpX= np.linspace(-1,1,100)
 # plt.plot(t,pX(t),'--')
-plt.legend(loc='best')
 plt.pause(10)
 plt.draw()
